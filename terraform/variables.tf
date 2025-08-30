1d1af573824a7b3f9c66e98a83bdb03cd174940b
chethan reddy <chethanreddymulli@gmail.com>
2025-08-30 12:12:29 +0530

    Update variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "linux_ami" { default = "ami-0c94855ba95c71c99" }
+variable "windows_ami" { default = "ami-0b2f6494ff0b07a0e" }
+variable "instance_type" { default = "t2.micro" }
+variable "key_name" { default = "your-key-name" }
